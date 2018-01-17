2ef39e1ff48a066dc839e8c996acb35f2f162dda
Hector Manuel Sedano Rodriguez <hectorse@ucm.es>
2018-01-18 00:29:20 +0100

    Intentando la colision bala enemigo. Me muero. Caidísimo. Pray, pray, pray

--- src/js/play_scene.js
+++ src/js/play_scene.js
@@ -1,10 +1,15 @@
 'use strict';
 
+var GO = require('./ship.js');
+
 var PlayScene = {
   create: function () {
-    var logo = this.game.add.sprite(
+    /*var logo = this.game.add.sprite(
       this.game.world.centerX, this.game.world.centerY, 'logo');
-    logo.anchor.setTo(0.5, 0.5);
+    logo.anchor.setTo(0.5, 0.5);*/
+
+    // INICIALIZACIÓN DE LAS FÍSICAS ARCADE
+    this.game.physics.startSystem(Phaser.Physics.ARCADE);
   }
 };
 
